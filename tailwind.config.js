e4f5078249b6c7c94226aa74195376917759c246
Nezerwa <eligrand2000@gmail.com>
2023-07-12 14:21:44 +0200

    Add html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: "'manrope', serif",
+      },
+      gridTemplateRows: {
+        layout: "1fr 3fr 60px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
